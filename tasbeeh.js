6af44f816a5f36fc82a38d7f3de8610769984006
ahmedmorad275 <ahmedmoradg30@gmail.com>
2025-10-19 23:33:21 +0300

    Used Local Storage

--- tasbeeh.js
+++ tasbeeh.js
@@ -2,22 +2,33 @@ const resetCount = document.getElementById('reset-count');
 const countElemnets = document.getElementsByClassName('countTasbeeh');
 const resetElemnets = document.getElementsByClassName('reset-current');
 
+for (let ele of countElemnets) {
+  ele.textContent = localStorage.getItem(`${ele.dataset.name}`) || '0';
+}
 // Reset All Counters
 resetCount.addEventListener('click', () => {
   for (let ele of countElemnets) {
     ele.textContent = '0';
     counter = 0;
+    localStorage.removeItem(`${ele.dataset.name}`);
   }
 });
 // Increase Counter
 for (let ele of countElemnets) {
   let counter = 0;
   ele.closest('div').addEventListener('click', () => {
-    if (ele.textContent === '0') {
+    if (
+      ele.textContent === '0' &&
+      localStorage.getItem(`${ele.dataset.name}`) === null
+    ) {
       counter = 0;
+    } else {
+      counter = localStorage.getItem(`${ele.dataset.name}`);
     }
     counter++;
+    navigator.vibrate(50);
     ele.textContent = counter;
+    localStorage.setItem(`${ele.dataset.name}`, `${ele.textContent}`);
   });
 }
 // Reset Only Clicked Counter
@@ -25,5 +36,6 @@ for (let ele of resetElemnets) {
   ele.addEventListener('click', () => {
     const targetedP = ele.parentElement.children[2].children[0];
     targetedP.textContent = '0';
+    localStorage.removeItem(`${targetedP.dataset.name}`);
   });
 }
